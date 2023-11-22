c0c1afca3f77a1654e00bd9778dd481f310284df
Henman2 <47754370+Henman2@users.noreply.github.com>
2023-11-22 06:21:30 +0000

    Fix Header and Footer components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import nucampLogo from '../app/assets/img/logo.png';
 
 const Header = ()=>{
     return(
         <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
             <NavbarBrand href='/'>
                 <img src={nucampLogo} alt='nucamp logo' />
             </NavbarBrand>
-        </Container>
     </Navbar>
     )
 }
