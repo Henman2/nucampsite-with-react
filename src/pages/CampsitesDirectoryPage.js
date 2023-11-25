0fbefefc6e86a6ef26068f3416cb7f077ec0bcbd
Henman2 <47754370+Henman2@users.noreply.github.com>
2023-11-25 14:44:41 +0000

    Add useState to display card details when selected

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,19 +1,17 @@
 import { useState } from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import CampsiteDetail from '../features/campsite/CampsiteDetail';
 import CampsitesList from '../features/campsite/CampsitesList';
-import {selectRandomCampsite} from '../features/campsite/campSitesSlice';
+import {selectCampsiteById} from '../features/campsite/campSitesSlice';
 
 const CampsitesDirectoryPage = () => {
-    const [selectedCampsite, toggleCampsite ] = useState(selectRandomCampsite());
+    const [campsiteId, setCampsiteId] = useState(0);
+    const selectedCampsite = selectCampsiteById(campsiteId);
     return (
         <Container>
-            <Button onClick={() => toggleCampsite(selectRandomCampsite())}>
-                Select Random Campsite
-            </Button>
             <Row>
                 <Col  sm='5' md='7'>
-                <CampsitesList />
+                <CampsitesList setCampsiteId={setCampsiteId} />
                 </Col>
                 <Col  sm='7' md='5'>
                 <CampsiteDetail campsite={selectedCampsite} />
