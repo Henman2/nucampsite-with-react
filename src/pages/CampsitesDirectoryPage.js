50567bafcbe657aed796acbc8392163c4bd02b47
Henman2 <47754370+Henman2@users.noreply.github.com>
2023-11-27 03:00:01 +0000

    Add Breadcomb Subheader

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -3,12 +3,14 @@ import { Container, Row, Col } from 'reactstrap';
 import CampsiteDetail from '../features/campsite/CampsiteDetail';
 import CampsitesList from '../features/campsite/CampsitesList';
 import {selectCampsiteById} from '../features/campsite/campSitesSlice';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
     const [campsiteId, setCampsiteId] = useState(0);
     const selectedCampsite = selectCampsiteById(campsiteId);
     return (
         <Container>
+            <SubHeader current='Directory'/>
             <Row>
                 <Col  sm='5' md='7'>
                 <CampsitesList setCampsiteId={setCampsiteId} />
