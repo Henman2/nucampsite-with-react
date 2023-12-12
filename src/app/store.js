94a58a309e3a93fd586d00dad7659f8cb23ade22
Henman2 <47754370+Henman2@users.noreply.github.com>
2023-12-12 10:29:15 +0000

    Add Redux campsitesSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// import counterReducer from '../features/counter/counterSlice';
+import { campsitesReducer } from '../features/campsite/campSitesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    // counter: counterReducer,
+    campsites: campsitesReducer
   },
 });
--- src/features/campsite/campSitesSlice.js
+++ src/features/campsite/campSitesSlice.js
@@ -1,6 +1,17 @@
 //file to handle global application state for campsites feature
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice } from '@reduxjs/toolkit';
 
+const initialState ={
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer; //export as reducer
 //return entire array of compasites
 export const selectAllCampsites = () => {
     return CAMPSITES;
